c49d238de4d8f35c76470715de91f861c4b4bfc2
williamchong <williamchong@users.noreply.github.com>
2025-12-09 03:23:09 +0000

    deploy: 43307e9034597316c1869ff1f9ba106ceec869ef

---
+++ _nuxt/CsFnR--5.js
@@ -0,0 +1,14 @@
+import{n as p,Y as h,t as l,p as g,O as n,s as u}from"./BvcX52ho.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"43307e9034597316c1869ff1f9ba106ceec869ef"}}catch{}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},r=new e.Error().stack;r&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[r]="9b49223d-e836-4f5c-9f7f-9133b323047f",e._sentryDebugIdIdentifier="sentry-dbid-9b49223d-e836-4f5c-9f7f-9133b323047f")})()}catch{}var c=Object.defineProperty,y=Object.getOwnPropertyDescriptor,i=(e,r,a,s)=>{for(var o=s>1?void 0:s?y(r,a):r,d=e.length-1,f;d>=0;d--)(f=e[d])&&(o=(s?f(r,a,o):f(o))||o);return s&&o&&c(r,a,o),o};let t=class extends p{constructor(){super(...arguments),this.size="1em",this.weight="regular",this.color="currentColor",this.mirrored=!1}render(){var e;return h`<svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="${this.size}"
+      height="${this.size}"
+      fill="${this.color}"
+      viewBox="0 0 256 256"
+      transform=${this.mirrored?"scale(-1, 1)":null}
+    >
+      ${t.weightsMap.get((e=this.weight)!=null?e:"regular")}
+    </svg>`}};t.weightsMap=new Map([["thin",l`<path d="M178.83,130.83l-80,80a4,4,0,0,1-5.66-5.66L170.34,128,93.17,50.83a4,4,0,0,1,5.66-5.66l80,80A4,4,0,0,1,178.83,130.83Z"/>`],["light",l`<path d="M180.24,132.24l-80,80a6,6,0,0,1-8.48-8.48L167.51,128,91.76,52.24a6,6,0,0,1,8.48-8.48l80,80A6,6,0,0,1,180.24,132.24Z"/>`],["regular",l`<path d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"/>`],["bold",l`<path d="M184.49,136.49l-80,80a12,12,0,0,1-17-17L159,128,87.51,56.49a12,12,0,1,1,17-17l80,80A12,12,0,0,1,184.49,136.49Z"/>`],["fill",l`<path d="M181.66,133.66l-80,80A8,8,0,0,1,88,208V48a8,8,0,0,1,13.66-5.66l80,80A8,8,0,0,1,181.66,133.66Z"/>`],["duotone",l`<path d="M176,128,96,208V48Z" opacity="0.2"/><path d="M181.66,122.34l-80-80A8,8,0,0,0,88,48V208a8,8,0,0,0,13.66,5.66l80-80A8,8,0,0,0,181.66,122.34ZM104,188.69V67.31L164.69,128Z"/>`]]);t.styles=g`
+    :host {
+      display: contents;
+    }
+  `;i([n({type:String,reflect:!0})],t.prototype,"size",2);i([n({type:String,reflect:!0})],t.prototype,"weight",2);i([n({type:String,reflect:!0})],t.prototype,"color",2);i([n({type:Boolean,reflect:!0})],t.prototype,"mirrored",2);t=i([u("ph-caret-right")],t);export{t as PhCaretRight};
