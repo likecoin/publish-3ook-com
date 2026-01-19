e0a27a4a660fce942589af5a78797f658b72dbc5
William Chong <me@williamchong.cloud>
2026-01-19 23:28:51 +0800

    👽️ Explicitly set irys node URL

--- utils/arweave.ts
+++ utils/arweave.ts
@@ -128,7 +128,7 @@ async function getBundler ({
   const p = await getProvider({ fileSize, ipfsHash, txHash, token })
   const { IS_TESTNET } = useRuntimeConfig().public
   const bundlr = new WebIrys({
-    network: IS_TESTNET ? 'devnet' : 'mainnet',
+    url: IS_TESTNET ? 'https://devnet.irys.network' : 'https://node1.irys.network',
     token: 'base-eth',
     wallet: {
       provider: p
