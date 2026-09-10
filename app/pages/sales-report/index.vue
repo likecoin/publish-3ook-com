3a059e12a8f74c8b9149dd6d316f53d0a9b97a74
Ng Wing Tat, David <i@ngwingt.at>
2026-09-10 14:41:10 +0800

    💄 Make sales report tabs sticky

--- app/pages/sales-report/index.vue
+++ app/pages/sales-report/index.vue
@@ -1,23 +1,19 @@
 <template>
   <PageContainer>
     <PageHeader :title="$t('menu.sales_report')" />
-    <PageBody>
+
+    <div class="sticky top-16 z-10 bg-default border-b border-default">
       <UTabs
-        v-model="selectedTabItemIndex"
-        class="w-full"
+        v-model="selectedTabValue"
         :items="tabItems"
-      >
-        <template #content="{ item }">
-          <SalesReportPlusReading
-            v-if="item.value === 'plus'"
-            :key="item.value"
-          />
-          <SalesReportCommissionHistory
-            v-else
-            :key="item.value"
-          />
-        </template>
-      </UTabs>
+        :content="false"
+        class="w-full max-w-5xl mx-auto px-4 py-3"
+      />
+    </div>
+
+    <PageBody>
+      <SalesReportPlusReading v-if="selectedTabValue === 'plus'" />
+      <SalesReportCommissionHistory v-else />
     </PageBody>
   </PageContainer>
 </template>
@@ -30,5 +26,5 @@ const tabItems = computed(() => [
   { label: $t('plus_reading_report.title'), value: 'plus' },
 ])
 
-const selectedTabItemIndex = ref('commission')
+const selectedTabValue = ref('commission')
 </script>
