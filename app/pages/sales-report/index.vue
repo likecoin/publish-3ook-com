9a7163584f233833f5b9dec043e4aadd87237305
Ng Wing Tat, David <i@ngwingt.at>
2026-09-10 14:42:29 +0800

    💄 Move payout history into its own tab

--- app/pages/sales-report/index.vue
+++ app/pages/sales-report/index.vue
@@ -13,6 +13,7 @@
 
     <PageBody>
       <SalesReportPlusReading v-if="selectedTabValue === 'plus'" />
+      <SalesReportPayoutHistory v-else-if="selectedTabValue === 'payout'" />
       <SalesReportCommissionHistory v-else />
     </PageBody>
   </PageContainer>
@@ -24,6 +25,7 @@ const { t: $t } = useI18n()
 const tabItems = computed(() => [
   { label: $t('user_settings.commission_history'), value: 'commission' },
   { label: $t('plus_reading_report.title'), value: 'plus' },
+  { label: $t('user_settings.payout_history'), value: 'payout' },
 ])
 
 const selectedTabValue = ref('commission')
